8505563f33dbb7972ba8941a9d3a7d9748a3209e
Baddewithana P <99246737+IT21247804@users.noreply.github.com>
2024-10-18 09:45:41 +0530

    feat(user authorization): add laravel breeze for user authorization.

--- LaravelOne/resources/views/layouts/layout.blade.php
+++ LaravelOne/resources/views/layouts/layout.blade.php
@@ -9,7 +9,7 @@
 <body>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">CRUD Application</a>
+        <a class="navbar-brand" href="#">Adminitrative tasks</a>
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
