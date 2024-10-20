a79caa404b82f7a9fd35d11f7e4df65f46699efa
Baddewithana P <99246737+IT21247804@users.noreply.github.com>
2024-10-20 15:15:02 +0530

    feat(initial-react): react-vite app initialization and changes to welcome.php

---
+++ LaravelOne/resources/js/components/App.jsx
@@ -0,0 +1,13 @@
+// resources/js/App.jsx
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+            <p>This is a simple React component inside a Laravel application.</p>
+        </div>
+    );
+};
+
+export default App;
